080a2e891ba3c59d1cdeed32f6e23d1038ea7584
Alexandre JOURNET <alexandre.journet@gmail.com>
2024-10-14 21:41:45 +0200

    chore: add robots.txt and sitemap.xml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,20 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning>
     <head>
         <title>Alexandre Journet | Portfolio</title>
+        <meta name="robots" content="all"/>
+        <meta
+            name="description"
+            content="Full-stack développeur depuis 5 ans. Voici mon parcours, mes expériences et mes projets. Il a été développé en Next.js et hébergé sur Github Pages."
+        />
+        <meta property="og:title" content="Alexandre Journet | Portfolio"/>
+        <meta
+            property="og:description"
+            content="Full-stack développeur depuis 5 ans. Voici mon parcours, mes expériences et mes projets. Il a été développé en Next.js et hébergé sur Github Pages."
+        />
+        <meta
+            property="og:image"
+            content="https://alexandrejournet.dev/assets/images/homepage.png"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet"/>
